006d1c4624440542a69ff4431b73f13ff0933fe6
Diego Fonseca <diego.fonseca@omnilatam.com>
2023-04-27 09:15:24 -0500

    Add celery instrumentor

--- klym_telemetry/instrumenters/__init__.py
+++ klym_telemetry/instrumenters/__init__.py
@@ -1,9 +1,11 @@
+from klym_telemetry.instrumenters.celery import _CeleryInstrumentor
 from klym_telemetry.instrumenters.django import _DjangoInstrumentor
 from klym_telemetry.instrumenters.fastapi import _FastAPIInstrumentor
 
 FACTORIES = {
     "fastapi": _FastAPIInstrumentor,
     "django": _DjangoInstrumentor,
+    "celery": _CeleryInstrumentor,
 }
 
 
